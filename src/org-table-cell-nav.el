f60130369cd8e4194b0eec4fa3651807c1dc3dc9
Ryan White <ryan@cognician.com>
2019-01-13 20:22:28 +0200

    First round of tweaks, documentation

--- src/org-table-cell-nav.el
+++ src/org-table-cell-nav.el
@@ -1,23 +1,29 @@
 ;; hopefully helps you zip around an org table, doing things...
 
-(defun org-table-cell-value ()
-	"Gives you the value of the cell. Always text - because you're in an org table."
-  (chomp (buffer-substring-no-properties (progn (search-backward "|" nil t) (forward-char) (point)) (progn (search-forward "|" nil t) (backward-char) (point)))))
-
-(defun org-table-cell-move-right (&optional count)
+(defun org-table-cell-go-right (&optional count)
 	"Move to the right. With no arguments, just one cell, with an argument (integer) - move that many cells right."
-      (search-forward "|" nil t count))
+	(dotimes (idx (or count 1)) (org-cycle)))
 
-(defun org-table-cell-move-left (&optional count)
+(defun org-table-cell-go-left (&optional count)
 	"Move to the left. With no arguments, just one cell, with an argument (integer) - move that many cells left."
-      (search-backward "|" nil t count)
-  (backward-char))
+	(dotimes (idx (or count 1)) (org-shifttab)))
 
-(defun org-table-cell-move-down (&optional count)
+(defun org-table-cell-go-down (&optional count)
 	"Move down. With no arguments, just one cell, with an argument (integer) - move that many cells down."
   (next-line count))
 
-(defun org-table-cell-move-up (&optional count)
+(defun org-table-cell-go-up (&optional count)
 	"Move up. With no arguments, just one cell, with an argument (integer) - move that many cells up."
   (previous-line count))
 
+(defun org-table-cell-chomp (str)
+  "Chomp leading and tailing whitespace from STR."
+  (replace-regexp-in-string (rx (or (: bos (* (any " \t\n")))
+                                    (: (* (any " \t\n")) eos)))
+                            ""
+                            str))
+
+(defun org-table-cell-value ()
+	"Gives you the value of the cell. Always text - because you're in an org table."
+	(save-excursion
+		(org-table-cell-chomp (buffer-substring-no-properties (progn (search-backward "|" nil t) (forward-char) (point)) (progn (search-forward "|" nil t) (backward-char) (point))))))
